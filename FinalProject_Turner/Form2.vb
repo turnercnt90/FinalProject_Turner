0838ea172b125097f77e1950ace07f79132f13db
turnercnt90 <turnercnt90@yahoo.com>
2016-11-14 17:19:47 -0500

    added code to form 2 for exit button

--- FinalProject_Turner/Form2.vb
+++ FinalProject_Turner/Form2.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form2
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Dim response As MsgBoxResult
+        response = MsgBox("Do you want to close form?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Confirm")
+        If response = MsgBoxResult.Yes Then
+            Me.Dispose()
+        ElseIf response = MsgBoxResult.No Then
+            Exit Sub
 
+        End If
+    End Sub
 End Class
