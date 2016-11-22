50299f1301fea86aba4b46e61f133cd9ac899df4
turnercnt90 <turnercnt90@yahoo.com>
2016-11-22 17:39:34 -0500

    Added database and table

---
+++ FinalProject_Turner/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customers Table]
+(
+	[Customer Name] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Phone] NVARCHAR(30) NULL, 
+    [date] DATE NOT NULL, 
+    [Service Performed] TEXT NOT NULL
+)
